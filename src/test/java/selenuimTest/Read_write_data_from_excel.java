322b0113d8031b9ab5eef3b221c6df769be32d78
ajeetkumar799 <ajeetkumarsingh799@gmail.com>
2026-06-02 18:47:22 +0530

    DataReadFromExcelsheetUtility

--- src/test/java/selenuimTest/Read_write_data_from_excel.java
+++ src/test/java/selenuimTest/Read_write_data_from_excel.java
@@ -9,37 +9,31 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class Read_write_data_from_excel {
-	public static String getDataFromExcelSheet(String filePath,String sheetName, int rowNo, int cellNo) throws IOException 
-	{
-		
-		String data = "";
-		FileInputStream file = new FileInputStream(filePath);
-		Workbook workbook = WorkbookFactory.create(file);
-	    Sheet sheet = workbook.getSheet(sheetName);
-         Row roww = sheet.getRow(rowNo);
-		  Cell cell = roww.getCell(cellNo);
-	
-			try {
-				data = cell.getStringCellValue();
-			}
-			catch(IllegalStateException e)
-			{
-				double value = cell.getNumericCellValue();
-			      //int intValue = (int) Math.round(value);
-				long doubleToLong = (long)value;
-				
-				String  longToString = String.valueOf(doubleToLong);
-				
-				data = longToString;
-			}
-			catch(NullPointerException e)
-			{
-				//System.out.println("Cell is Blank");
-			}
-		
-	    workbook.close();
-	    return data;   
-}
+	public static String getDataFromExcelSheet(String filePath, String sheetName, int rowNo, int cellNo) throws IOException {
+	    String data = "";
+
+	    try (FileInputStream file = new FileInputStream(filePath);
+	         Workbook workbook = WorkbookFactory.create(file)) {
+
+	        Sheet sheet = workbook.getSheet(sheetName);
+	        Row row = sheet.getRow(rowNo);
+
+	        if (row != null) {
+	            Cell cell = row.getCell(cellNo);
+
+	            if (cell != null) {
+	                try {
+	                    data = cell.getStringCellValue();
+	                } catch (IllegalStateException e) {
+	                    double value = cell.getNumericCellValue();
+	                    data = String.valueOf(value);
+	                }
+	            }
+	        }
+	    }
+
+	    return data;
+	}
 
 	public static void main(String[] args) throws IOException {
 		String data =getDataFromExcelSheet("D:\\ajeet\\Documents\\Book1.xlsx\\","Sheet1", 1, 0);
